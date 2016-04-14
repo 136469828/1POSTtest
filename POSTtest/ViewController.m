bdb07979f00435159660d26119813f21ccc94264
136469828 <136469828@qq.com>
2016-04-14 15:27:14 +0800

    test
    teat

--- POSTtest/ViewController.m
+++ POSTtest/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "AFNetworking.h"
 @interface ViewController ()
 
 @end
@@ -17,11 +17,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self test];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (void)test
+{
+    AFHTTPRequestOperationManager *manger = [AFHTTPRequestOperationManager manager];
+    NSDictionary *parameters = @{
+                                    @"_appid":@"101",
+                                    @"_code":@"bf1ced86a7ab4efbbb0f310f99f0097a",
+//                                    @"content":@"application/json",
+//                                     @"UserName": @"sample string 1",
+//                                     @"Password": @"sample string 2",
+                                 };
+    //@"http://192.168.1.4:88/common/user/login"
+    NSString *url = @"http://192.168.1.4:88/common/advertise/getlist";
+    [manger POST:url parameters:parameters success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
+        id data = responseObject[@"Data"];
+        
 
+        
+    } failure:^(AFHTTPRequestOperation * _Nonnull operation, NSError * _Nonnull error) {
+        NSLog(@"error : %@",error);
+    }];
+    
+}
 @end
